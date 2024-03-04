72dcab5ff8516f3c9008460018a7718e4d209896
GouthamGuna <ggowthamsankar@gmail.com>
2024-03-04 08:46:03 +0530

    initial commit

---
+++ src/main/java/in/dev/gmsk/CoreJavaCollectionsApplication.java
@@ -0,0 +1,7 @@
+package in.dev.gmsk;
+
+public class CoreJavaCollectionsApplication {
+    public static void main(String[] args) {
+        System.out.println( "Hello Lunar!" );
+    }
+}
---
+++ src/main/java/in/dev/gmsk/synchronizedcollections/SynchronizedApplication.java
@@ -0,0 +1,31 @@
+package in.dev.gmsk.synchronizedcollections;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+public class SynchronizedApplication {
+
+    static Collection<Integer> syncCollection =
+            Collections.synchronizedCollection( new ArrayList<>() );
+
+    static Runnable listOperations = () -> {
+        syncCollection.addAll( Arrays.asList( 1, 2, 3, 4, 5, 6 ) );
+    };
+
+    public static void main(String[] args) {
+
+        try {
+            Thread thread = new Thread( listOperations );
+            thread.start();
+            thread.join();
+
+            System.out.println("syncCollection.size() : "+syncCollection.size());
+            syncCollection.forEach((e) -> System.out.println( "e = " + e ));
+
+        } catch (InterruptedException e) {
+            throw new RuntimeException( e );
+        }
+    }
+}
