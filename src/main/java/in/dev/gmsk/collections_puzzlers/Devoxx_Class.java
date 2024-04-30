b1822651f1cf44841b0945ee2979a5ffe4d37686
GouthamGuna <ggowthamsankar@gmail.com>
2024-04-30 05:41:35 +0530

    Update Devoxx_Class.java

--- src/main/java/in/dev/gmsk/collections_puzzlers/Devoxx_Class.java
+++ src/main/java/in/dev/gmsk/collections_puzzlers/Devoxx_Class.java
@@ -5,13 +5,17 @@ import java.util.stream.Stream;
 
 import static java.lang.System.*;
 
+/**
+ * @see <a href="https://youtu.be/w6hhjg_gt_M?si=07So8N_JQTh4Q_7Z">Ref : Java Collections Puzzlers by Maurice Naftalin And José Paumard</a>
+ */
+
 public class Devoxx_Class {
 
     public static void main(String[] args) {
         _1_arrays_As_List();
         _2_Sublist();
-       // _3a_HandlingNulls();
-       // _3b_HandlingNulls();
+        // _3a_HandlingNulls();
+        // _3b_HandlingNulls();
         _4a_MultiMap();
     }
 
@@ -27,18 +31,18 @@ public class Devoxx_Class {
         out.print(intOfList.contains(1)); // false
     }
 
-    private static void _2_Sublist(){
+    private static void _2_Sublist() {
 
         var ints = new ArrayList<>(List.of(1, 2, 3, 4, 5));
 
-        var subList = ints.subList(0,0);
+        var subList = ints.subList(0, 0);
         out.print("\nsubList = " + subList);
 
         subList.addAll(List.of(10, 11, 12));
         out.print("\tints = " + ints);
     }
 
-    private static void _3a_HandlingNulls(){
+    private static void _3a_HandlingNulls() {
 
         String[] strings = {"a", "b", "c", null};
 
@@ -49,7 +53,7 @@ public class Devoxx_Class {
         out.print("\tlistOne size = " + listOne); // null pointer error came...
     }
 
-    private static void _3b_HandlingNulls(){
+    private static void _3b_HandlingNulls() {
 
         String[] strings = {"a", "b", "c", null};
 
@@ -58,14 +62,14 @@ public class Devoxx_Class {
         out.println("\nlist size = " + list.size()); // java.lang.UnsupportedOperationException
     }
 
-    private static void _4a_MultiMap(){
+    private static void _4a_MultiMap() {
 
         Map<Integer, String> map = new HashMap<>();
         map.put(1, null);
 
-        out.print("\ndefaultVal = "+map.getOrDefault(1, "Nothing..."));
+        out.print("\ndefaultVal = " + map.getOrDefault(1, "Nothing..."));
 
         map.putIfAbsent(1, "One");
-        out.print("\tgetVal = "+map.get(1));
+        out.print("\tgetVal = " + map.get(1));
     }
 }
