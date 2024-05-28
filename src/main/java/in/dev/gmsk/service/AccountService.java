c19f1c2c9298241a72e7d7e43831f397cdcfa5bb
GouthamGuna <ggowthamsankar@gmail.com>
2024-05-28 18:14:34 +0530

    Update AccountService.java & AccountServiceImpl.java

--- src/main/java/in/dev/gmsk/service/AccountService.java
+++ src/main/java/in/dev/gmsk/service/AccountService.java
@@ -2,10 +2,13 @@ package in.dev.gmsk.service;
 
 import in.dev.gmsk.util.JDBCConnection;
 import in.dev.gmsk.util.model.Accounts;
+import in.dev.gmsk.util.model.FeeModel;
 
 import java.util.stream.Stream;
 
 public interface AccountService {
 
     Stream<Accounts> findByAll(JDBCConnection connection);
+
+    Stream<FeeModel> getDefaulterFeeList (FeeModel model, JDBCConnection connection);
 }
